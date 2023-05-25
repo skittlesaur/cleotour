4c1aa1651c3d61c8846bd566d3310f76f59e7d0b
sbayce <youssef2311@gmail.com>
2023-05-25 17:52:21 +0300

    added Trending Section widget

--- lib/widgets/common/trendingSection.dart
+++ lib/widgets/common/trendingSection.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'trendingCard.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class TrendingSection extends StatelessWidget {
   @override
@@ -13,20 +14,34 @@ class TrendingSection extends StatelessWidget {
           child: Text(
             "Trending Tours",
             style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white),
+                fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black),
           ),
         ),
         Container(
           height: 300,
-          child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
+          child: ListView(
+            children: [
+              CarouselSlider(
+                items: [
+                  TrendingCard(),
+                  TrendingCard(),
                   TrendingCard(),
                   TrendingCard(),
                   TrendingCard(),
                 ],
-              )),
+                options: CarouselOptions(
+                  height: 280.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 1,
+                ),
+              ),
+            ],
+          ),
         )
       ],
     );
