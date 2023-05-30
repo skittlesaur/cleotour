eb6d1843bc88cf2ee8520e857993b2b7d54568c8
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-30 19:29:07 +0300

    last commits

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,25 @@ import 'package:cleotour/widgets/top-bar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'screens/home.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-        appId: '1:277688353042:android:95f586db4aa1556a3a33cb',
-        apiKey: 'AIzaSyBha9BBSHOLFjkIBuKJOCinYYWCOqUV2Gk',
-        projectId: 'cleotour-8bd53',
-        messagingSenderId: '277688353042',
-        storageBucket: 'cleotour-8bd53.appspot.com/'),
-  );
-
+      // options: FirebaseOptions(
+      //     appId: '1:277688353042:android:95f586db4aa1556a3a33cb',
+      //     apiKey: 'AIzaSyBha9BBSHOLFjkIBuKJOCinYYWCOqUV2Gk',
+      //     projectId: 'cleotour-8bd53',
+      //     messagingSenderId: '277688353042',
+      //     storageBucket: 'cleotour-8bd53.appspot.com/'),
+      );
+  FirebaseMessaging.onBackgroundMessage(background_notif_handler);
   runApp(MyApp());
+}
+
+Future<void> background_notif_handler(RemoteMessage message) async {
+  print("Handling a background message: ${message.data}");
 }
 
 class MyApp extends StatefulWidget {
